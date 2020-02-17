3909a1b6346fb572ec1fbee80d63c8094da86d64
Lavrenova <olavrenova@baltbet.ru>
2020-02-17 17:55:17 +0300

    Added service

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ProductsService } from '../products.service'
 
 @Component({
   selector: 'app-product',
@@ -9,14 +10,14 @@ export class ProductComponent implements OnInit {
     @Input() productName: string;
     @Output() productClicked = new EventEmitter();
 
-  constructor() { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
   }
 
   onClicked() {
-      console.log('++++')
-      this.productClicked.emit();
+      // this.productClicked.emit();
+      this.productsService.deleteProduct(this.productName)
   }
 
 }
